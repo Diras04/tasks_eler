16de2db8c4ae0b51573a58c796670c5647a4914f
Diras04 <derkach.andrey0486@gmail.com>
2025-02-28 14:48:16 +0200

    2

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,14 @@
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-    public static int recurs(int a, int b) {
-        if (b == 0)
-            return a;
-        if (b > 0) {
-            return recurs(a + 1, b - 1);
-        } else return recurs(a - 1, b + 1);
-    }
 
     public static void main(String[] args) {
-
-
-        System.out.println(recurs(25, 13));
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
+        System.out.println(string.length());
 
 
     }
