74b459e40a188ffd8d5ddd0b08161e200655cc47
Diras04 <derkach.andrey0486@gmail.com>
2025-02-28 14:07:14 +0200

    recurc

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,18 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
+    public static int recurs(int a, int b) {
+        if (b == 0)
+            return a;
+        if (b > 0) {
+            return recurs(a + 1, b - 1);
+        } else return recurs(a - 1, b + 1);
+    }
 
     public static void main(String[] args) {
-        int a = 5;
-        int b = 6;
-        int sum = 0;
 
-        while (a>0){
-            sum++;
-            a--;
-        }
-        while (b>0){
-            sum++;
-            b--;
-        }
-        System.out.println(sum);
+
+        System.out.println(recurs(25, 13));
 
 
     }
